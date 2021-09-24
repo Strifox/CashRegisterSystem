c71b47fcb828e80abc855bd9f07620e8258aac66
Johan Svensson <johan.svensson93@outlook.com>
2021-09-24 15:31:59 +0200

    Models added
    Added models for Blu-Ray and Dvd movies and Customer.

---
+++ CashRegisterSystem/Models/DvdMovie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashRegisterSystem.Models
+{
+    public class DvdMovie
+    {
+        public int Price { get; set; }
+        public int Discount { get; set; }
+
+    }
+}
