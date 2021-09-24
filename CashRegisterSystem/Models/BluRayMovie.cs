e9e3612d6cf4b05b0303b56ed56754f1ef12bb19
Johan Svensson <johan.svensson93@outlook.com>
2021-09-24 15:46:34 +0200

    Discount added
    Added discount method to model classes.

--- CashRegisterSystem/Models/BluRayMovie.cs
+++ CashRegisterSystem/Models/BluRayMovie.cs
@@ -7,5 +7,17 @@ namespace CashRegisterSystem.Models
 {
     public class BluRayMovie
     {
+        public int Price { get; set; }
+        public int Discount { get; set; }
+
+        /// <summary>
+        /// Removes 15 percent on price
+        /// </summary>
+        /// <param name="price">Blu-Ray price</param>
+        /// <returns>Discounted price</returns>
+        public double BluRayDiscount(double price)
+        {
+            return price * 0.85;
+        }
     }
 }
--- CashRegisterSystem/Models/DvdMovie.cs
+++ CashRegisterSystem/Models/DvdMovie.cs
@@ -10,5 +10,15 @@ namespace CashRegisterSystem.Models
         public int Price { get; set; }
         public int Discount { get; set; }
 
+        /// <summary>
+        /// Removes 15 percent on price
+        /// </summary>
+        /// <param name="price">Dvd price</param>
+        /// <returns>Discounted Price</returns>
+        public double DvdDiscount(double price)
+        {
+            return price * 0.90;
+        }
+
     }
 }
