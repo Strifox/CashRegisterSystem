f8c59d77417a9a22e898dbdd4c806704568f9c6d
Johan Svensson <johan.svensson93@outlook.com>
2021-09-24 20:01:11 +0200

    Removed old view and added new one

---
+++ CashRegisterSystem/Controllers/RentalController.cs
@@ -0,0 +1,91 @@
+﻿using CashRegisterSystem.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CashRegisterSystem.Controllers
+{
+    public class RentalController : Controller
+    {
+        // GET: Rental
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Rental/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Rental/Create
+        public ActionResult Create([Bind("Name, Membership, DvdMovies, BluRayMovies")] Customer customer)
+        {
+
+            return View();
+        }
+
+        // POST: Rental/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Rental/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Rental/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Rental/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Rental/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+    }
+}
